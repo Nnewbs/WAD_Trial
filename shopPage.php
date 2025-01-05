8989b260f4d27b7c5cc25d1f81f7af3d340067e6
Nnewbs <110174101+Nnewbs@users.noreply.github.com>
2025-01-05 11:35:25 +0800

    shoppage

--- shopPage.php
+++ shopPage.php
@@ -4,6 +4,7 @@
 
 <?php
     $id = $_GET['id'];
+    
     $sql = "SELECT * FROM users WHERE email = '$id'";
     $query = mysqli_query($dbconn, $sql) or die("Error: " . mysqli_error($dbconn));
     $row = $query -> fetch_assoc();
