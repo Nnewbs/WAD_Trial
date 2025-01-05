6c4edf992073eec62f02a1f8c2b6fbe725d4c3c1
Nnewbs <110174101+Nnewbs@users.noreply.github.com>
2025-01-05 12:53:02 +0800

    admin

---
+++ deleteProduct.php
@@ -0,0 +1,14 @@
+<?php
+include("dbconn.php");
+
+$id = $_GET['id'];  // Fetch the item ID from the URL
+
+// Deleting the product
+$sql = "DELETE FROM products WHERE id = '$id'";
+
+if ($dbconn->query($sql) === TRUE) {
+    echo "<script>alert('The product has been successfully deleted!'); window.location = 'adminMenuMan.php';</script>";
+} else {
+    echo "<script>alert('Failed to delete the product data. Please try again!'); window.location = 'adminMenuMan.php';</script>";
+}
+?>
